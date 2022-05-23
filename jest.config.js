5b5cad2d31d615bef99f10e7ef3a4e2a579d6da8
Juan Agustín <aguseme01@outlook.com>
2022-05-23 15:28:45 -0300

    Fix unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,29 @@
-const ROOT_DIR_SRC = "<rootDir>/src/$1"
-module.exports = {
-    moduleFileExtensions: ["js", "json", "vue"],
-    moduleNameMapper: {
-        ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-        "identity-obj-proxy",
-        "^~/(.*)$": ROOT_DIR_SRC,
-        "^~~/(.*)$": ROOT_DIR_SRC,
-        "^@/(.*)$": ROOT_DIR_SRC,
-        "^@@/(.*)$": ROOT_DIR_SRC,
-        "^@./(.*)$": "<rootDir>/$1"
-    },
-    transform: {
-        "^.+\\.js$": "babel-jest",
-        "^.+\\.vue$": "@vue/vue3-jest"
-        
-    },
-    snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ['<rootDir>/jest.config.js']
-}
+/**
+ * 🃏 Jest configuration file
+ * https://jestjs.io/docs/27.x/getting-started
+ */
+ const ROOT_DIR_SRC = "<rootDir>/src/$1"
+ module.exports = {
+     moduleFileExtensions: ["js", "json", "vue"],
+     moduleNameMapper: {
+         ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "identity-obj-proxy",
+         "^~/(.*)$": ROOT_DIR_SRC,
+         "^~~/(.*)$": ROOT_DIR_SRC,
+         "^@/(.*)$": ROOT_DIR_SRC,
+         "^@@/(.*)$": ROOT_DIR_SRC,
+         "^@./(.*)$": "<rootDir>/$1",
+         "^@root(.*)$": ROOT_DIR_SRC,
+         "^@components(.*)$": "<rootDir>/src/components$1",
+         "^@pages(.*)$": "<rootDir>/src/pages$1",
+         "^@scripts(.*)$": "<rootDir>/src/assets/scripts$1",
+         "^@stores(.*)$": "<rootDir>/stores/$1"
+     },
+     transform: {
+         "^.+\\.js$": "babel-jest",
+         "^.+\\.vue$": "@vue/vue3-jest"
+ 
+     },
+     snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
+     testEnvironment: "jsdom",
+     setupFilesAfterEnv: ['<rootDir>/jest.config.js']
+ }
